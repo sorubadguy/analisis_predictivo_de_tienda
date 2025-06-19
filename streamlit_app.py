d6da6c07da0fc45c2a1ae04d090898d7a14bcdd3
sorubadguy <lucasmartinez33@gmail.com>
2025-06-19 01:08:58 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import io
 
